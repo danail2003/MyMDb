b922d84e9739c95de37de7129d4ee17d01bf9ea7
danail2003 <ddobrev20@outlook.com>
2022-08-17 14:10:34 +0300

    Added messages to post-movie form validations

--- MyMDb/Client/src/app/feature/movies/post-movie-page/post-movie-page.component.ts
+++ MyMDb/Client/src/app/feature/movies/post-movie-page/post-movie-page.component.ts
@@ -17,9 +17,9 @@ export class PostMoviePageComponent implements OnInit {
     'description': new FormControl('', [Validators.required, Validators.minLength(10)]),
     'releaseDate': new FormControl('', [Validators.pattern(/^(1[012]|0[1-9])([\/\-])(0[1-9]|[12]\d|3[01])\2((?:19|20)?\d{2})$/g)]),
     'year': new FormControl('', [Validators.required, Validators.min(1888), Validators.max(2023)]),
-    'duration': new FormControl(''),
-    'budget': new FormControl(''),
-    'gross': new FormControl(''),
+    'duration': new FormControl('', [Validators.required]),
+    'budget': new FormControl('', [Validators.required]),
+    'gross': new FormControl('', [Validators.required]),
     'rating': new FormControl('', [Validators.required, Validators.min(1), Validators.max(10)]),
     'video': new FormControl('', [Validators.required, Validators.pattern(/^http/g)]),
     'image': new FormControl('', [Validators.required, Validators.pattern(/^http/g)])
